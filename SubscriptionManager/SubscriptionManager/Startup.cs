0b46a8be9249e9391be23d792a55cefc12814d79
migimasa <migimasa@gmail.com>
2015-08-25 20:36:23 -0500

    Create Project

---
+++ SubscriptionManager/SubscriptionManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SubscriptionManager.Startup))]
+namespace SubscriptionManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
